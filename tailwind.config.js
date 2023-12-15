ec249c07824107b6d4ffdc47fb8ad8ccbe91e955
elbob-mx <rob7th@gmail.com>
2023-12-15 15:14:51 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      small: "0px",
+      mobile: "359px",
+      mobileHor: "640px",
+      tablet: "768px",
+      laptop: "976px",
+      ipad: "1023px",
+      hd: "1365px",
+      fhd: "1440px",
+    },
+    extend: {
+      colors: {
+        azulAgro: "#1755A1",
+        azulAgroClaro: "#00ABEA",
+        offWhite: "#F2F2F2",
+        offBlack: "#181818",
+      },
+    },
+  },
+  plugins: ["flowbite/plugin"],
+};
